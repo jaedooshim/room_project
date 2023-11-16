8a1d2beba98ff71e0939854f0f52445c6f7af52f
shimjaedoo <woen1637@naver.com>
2023-11-16 17:52:13 +0900

    feat: prisma softdelete Middleware

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,9 +1,27 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { Prisma, PrismaClient } from '@prisma/client';
 
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
     await this.$connect();
+    await this.$use(async (params, next) => await this.softDelete(params, next));
+  }
+
+  async softDelete(params: Prisma.MiddlewareParams, next) {
+    if (params.action == 'delete') {
+      params.action = 'update';
+      params.args['data'] = { deletedAt: new Date() };
+    }
+    if (params.action == 'deleteMany') {
+      params.action = 'updateMany';
+      if (params.args.data != undefined) {
+        params.args.data['deletedAt'] = new Date();
+      } else {
+        params.args['data'] = { deletedAt: new Date() };
+      }
+    }
+
+    return next(params);
   }
 }
