5899b208eb9290dbc0a5be37a7ee37cc4b9cf41e
shimjaedoo <woen1637@naver.com>
2023-11-17 01:22:17 +0900

    feat: IFindUser Interface

--- src/user/user.interface.ts
+++ src/user/user.interface.ts
@@ -1,4 +1,8 @@
 import { User } from '@prisma/client';
-
+import { User as PrismaUser } from '@prisma/client';
 export interface ICreateUser
   extends Pick<User, 'name' | 'nickname' | 'password' | 'email' | 'tel' | 'address' | 'subAddress' | 'role'> {}
+
+export interface IFindUser extends PrismaUser {
+  Id?: string;
+}
