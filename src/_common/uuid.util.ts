c8d7cf8cf89fe8d47c5400d3144b2f31f60ec0f4
shimjaedoo <woen1637@naver.com>
2023-11-16 15:53:42 +0900

    chore: Base62, uuid

---
+++ src/_common/uuid.util.ts
@@ -0,0 +1,32 @@
+import { v4 as uuidv4, parse as uuidParse, stringify as uuidStringify } from 'uuid';
+import * as Base62 from 'base62/lib/ascii';
+
+export class UuidUtil {
+  static toBase62(uuid: string): string {
+    if (!this.isValidUuid(uuid)) {
+      throw new Error('유효하지않은 UUID 입니다.');
+    }
+    const bytes = uuidParse(uuid);
+    return Base62.encode(bytes);
+  }
+
+  static fromBase62(encoded: string): string {
+    if (!this.isValidBase62(encoded)) {
+      throw new Error(' 유효하지 않은 Base62 값입니다.');
+    }
+    const bytes = Base62.decode(encoded);
+    return uuidStringify(bytes);
+  }
+
+  static generateUuid(): string {
+    return uuidv4();
+  }
+
+  static isValidUuid(uuid: string): boolean {
+    return /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/.test(uuid);
+  }
+
+  static isValidBase62(str: string): boolean {
+    return /^[0-9a-zA-Z]+$/.test(str);
+  }
+}
