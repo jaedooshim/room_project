e58c27973cc2d6e0e180b7c951e7920dbe545686
shimjaedoo <woen1637@naver.com>
2023-12-07 19:27:48 +0900

    chore: 모듈추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
-
+import { AgentModule } from './agent/agent.module';
+import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import * as process from 'process';
+console.log(process.env);
 @Module({
-  imports: [UserModule, PrismaModule],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), UserModule, PrismaModule, AgentModule, AuthModule],
   controllers: [],
   providers: [],
 })
