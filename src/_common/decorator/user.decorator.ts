ce978baa66a7041836da0496c5dcf977a051e6df
shimjaedoo <woen1637@naver.com>
2023-12-07 19:31:49 +0900

    feat: 유저 커스텀 데코레이터

--- src/_common/decorator/user.decorator.ts
+++ src/_common/decorator/user.decorator.ts
@@ -1,6 +1,7 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { User } from '@prisma/client';
 
+// 유저 커스텀 데코레이터
 export const DUser = createParamDecorator((data, ctx: ExecutionContext): User => {
   const req = ctx.switchToHttp().getRequest();
   return req.user;
